3c86ef835f388d1bd9495b229e24c66f46604a8d
Salman-Inayat <salmaninayat1999@gmail.com>
2020-06-20 23:26:39 +0500

    Intro to Redux

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,7 +3,7 @@ import {Reducer, initialState} from "./reducer";
 
 export const ConfigureStore = () => {
     const store = createStore(
-        Reducer,
+        Reducer, // Reducer pure function
         initialState // initial state
     );
     return store;
