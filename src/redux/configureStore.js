b4787e9e6d2d10438899bcf0703a27a0a76a1c85
Salman-Inayat <salmaninayat1999@gmail.com>
2020-06-25 18:29:13 +0500

    Final project

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,7 +2,7 @@ import {createStore, combineReducers, applyMiddleware} from 'redux';
 import {Dishes} from "./dishes";
 import {Comments} from "./comments";
 import {Promotions} from "./promotions";
-import {Leaders} from "./leaders";
+import {Leaders} from './leaders';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { createForms } from 'react-redux-form';
@@ -11,16 +11,20 @@ import { InitialFeedback } from './forms';
  
 export const ConfigureStore = () => {
     const store = createStore(
-        combineReducers({
-            dishes: Dishes,
-            comments: Comments,
-            promotions: Promotions,
-            leaders: Leaders,
-            ...createForms({
-                feedback: InitialFeedback
-            })
-        }),
-        applyMiddleware(thunk, logger)
+        combineReducers(
+            {
+                dishes:Dishes,
+                comments:Comments,
+                promotions:Promotions,
+                leaders: Leaders,
+                ...createForms(
+                    {
+                        feedback:InitialFeedback
+                    }
+                )
+            }
+        ),
+        applyMiddleware(thunk,logger)
     );
     return store;
 }
